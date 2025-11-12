9242eacd57482c094a6d76cd69eb5e680173fe5e
diamondxorb <juliaamorimp@gmail.com>
2025-11-12 18:42:04 -0300

    mais jetpunk

--- Vowels/vowel.py
+++ Vowels/vowel.py
@@ -1,7 +1,18 @@
+"""
+    Programa inspirado no puzzle Countries With the Highest Percentage of Vowels do Jetpunk
+    @https://www.jetpunk.com/user-quizzes/287534/countries-with-the-highest-vowel-to-consonant-ratio
+
+    A partir de um arquivo contendo a lista de países do mundo ele calcula as vogais e consoantes e gera a
+    porcentagem, cria um dicionário com o nome e a porcentagem e por fim ordena eles.
+    Por algum motivo, o puzzle considera Y como vogal dependendo da pronúncia fonética em inglês, o que é bem
+    ridículo, então está como comentário no código, mas a resposta oficial considera Y.
+
+"""
+
 def vowelOrConsonant(char):
     char = char.lower()
 
-    if(char=="a" or char=="e" or char=="i" or char=="o" or char=="u"):
+    if(char=="a" or char=="e" or char=="i" or char=="o" or char=="u"): #or char=="y"
         return 1
     else:
         return 0
